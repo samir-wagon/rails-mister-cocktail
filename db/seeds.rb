4de552d80d66e23bab9b9707918ab50b4d0c7b13
samir aissioui <samir.aissioui@sciencespo.fr>
2019-02-14 17:34:22 +0100

    update exo 2

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,35 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# require 'json'
+# require 'open-uri'
+
+# puts 'Cleaning database...'
+# Ingredient.destroy_all
+
+# puts 'Creating ingredients...'
+
+# url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+# ingredient_serialized = open(url).read
+# user = JSON.parse(ingredient_serialized)
+
+# puts "Finished, we have #{Ingredient.count} ingredientss in our list!"
+
+puts 'Cleaning database...'
+Ingredient.destroy_all
+
+puts 'Creating ingredients...'
+Ingredient.create(name: "lemon")
+Ingredient.create(name: "ice")
+Ingredient.create(name: "mint leaves")
+
+Cocktail.create(name: "Le tropico")
+Cocktail.create(name: "Picolo")
+Cocktail.create(name: "Long island")
+
+Dose.create(description: 5, ingredient_id: 1, cocktail_id: 1)
+Dose.create(description: 2, ingredient_id: 2, cocktail_id: 1)
+Dose.create(description: 1, ingredient_id: 3, cocktail_id: 1)
+
+puts "Finished, we have #{Ingredient.count} ingredients & #{Cocktail.count} cocktails in our list!"
